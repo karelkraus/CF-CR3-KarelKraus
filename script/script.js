918d522f3a844e9da23f2cf00e2d1675e9f2d401
Karel Kraus <kaja.kraus@gmail.com>
2021-02-08 12:39:43 +0100

    updated JS and inputs

--- script/script.js
+++ script/script.js
@@ -1,18 +1,25 @@
 function calculation () {
+
 	var yourName = document.getElementById("yourName").value;
 	var age = document.getElementById("age").value;
 	var country = document.getElementById("country").value;
 	var horsepower = document.getElementById("horsepower").value;
 
-	if (country == "Austria") {
-		var cost = Math.floor(Number(horsepower) * 100 / Number(age) + 50);
-		document.getElementById("result").innerHTML = yourName + ", your insurance costs " + cost + " &euro;";
-	} else if (country == "Greece") {
-		var cost = Math.floor(Number(horsepower) * 120 / Number(age) + 100);
-		document.getElementById("result").innerHTML = yourName + ", your insurance costs " + cost + " &euro;";
-	} else if (country == "Hungary") {
-		var cost = Math.floor(Number(horsepower) * 150 / (Number(age)+3) + 50);
-		document.getElementById("result").innerHTML = yourName + ", your insurance costs " + cost + " &euro;";
+	if(!horsepower || !age || yourName == "") {
+		document.getElementById("result").innerHTML = "Fill all fields";
+	} else {
+
+		if (country == "Austria") {
+			var cost = Math.floor(Number(horsepower) * 100 / Number(age) + 50);
+			document.getElementById("result").innerHTML = yourName + ", your insurance costs " + cost + " &euro;";
+		} else if (country == "Greece") {
+			var cost = Math.floor(Number(horsepower) * 120 / Number(age) + 100);
+			document.getElementById("result").innerHTML = yourName + ", your insurance costs " + cost + " &euro;";
+		} else if (country == "Hungary") {
+			var cost = Math.floor(Number(horsepower) * 150 / (Number(age)+3) + 50);
+			document.getElementById("result").innerHTML = yourName + ", your insurance costs " + cost + " &euro;";
+		}
+
 	}
 }
 
